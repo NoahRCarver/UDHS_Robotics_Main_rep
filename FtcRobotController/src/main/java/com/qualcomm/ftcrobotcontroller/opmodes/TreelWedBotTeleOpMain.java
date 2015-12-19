f275041d0aadcb51dfb8493d3fcda6848d2d04d1
Scientific.noah@gmail.com <3fp7()Z1>
2015-12-19 12:49:19 -0500

    moar werk

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TreelWedBotTeleOpMain.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TreelWedBotTeleOpMain.java
@@ -3,11 +3,16 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class TreelWedBotTeleOpMain extends OpMode {
 
-    DcMotor FRM, BRM;
-    DcMotor FLM, BLM;
+    DcMotor FRM, BRM; //Front Right Motor, Back Right Motor
+    DcMotor FLM, BLM; //Front Left Motor, Back Left Motor
+    DcMotor TDM, TIM; //Tread Delivery Motor, Tread Intake Motor
+    Servo RP, LP, CA, BTN; // Right Paddle, Left paddle, Climber Arm, Button pusher.
+
+
     BaseDriveMotorControl driveScheme;
 
     public TreelWedBotTeleOpMain() {}
@@ -26,9 +31,19 @@ public class TreelWedBotTeleOpMain extends OpMode {
         BRM = hardwareMap.dcMotor.get("br");
         FLM = hardwareMap.dcMotor.get("fl");
         BLM = hardwareMap.dcMotor.get("bl");
+
+        TDM = hardwareMap.dcMotor.get("out");
+        TIM = hardwareMap.dcMotor.get("in");
+
+        RP = hardwareMap.servo.get("rp");
+        LP = hardwareMap.servo.get("lp");
+        CA = hardwareMap.servo.get("ca");
+        BTN = hardwareMap.servo.get("btn");
         
         //init driveScheme
         driveScheme = new BaseDriveMotorControl(FRM, BRM, FLM, BLM);
+
+
 
     }
     
@@ -41,6 +56,23 @@ public class TreelWedBotTeleOpMain extends OpMode {
     @Override
     public void loop() {
         driveScheme.tankDrive(gamepad1);
+
+        if(gamepad2.a){
+            TIM.setPower(1);
+        }else{
+            TIM.setPower(0);
+        }
+
+        if(gamepad2.dpad_left){
+            TDM.setPower(.25);
+        }else if(gamepad2.dpad_right){
+            TDM.setPower(-.25);
+        }else{
+            TDM.setPower(0);
+        }
+
+
+
     }
 
     @Override
